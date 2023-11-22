74058b1518ac97fec654bf5e5d2742c3edd10b80
Fayaj Hossain <fayazhossain88@gmail.com>
2023-11-22 17:20:01 +0530

    Fix MongoDB connection and add token generation methods

--- chai-aur-backend/chai-backend/src/db/index.js
+++ chai-aur-backend/chai-backend/src/db/index.js
@@ -1,13 +1,13 @@
-import mongoose, { connect } from "mongoose";
+import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`,
+      `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
-      `\n MongoDB connected !! DB Host: ${connectionInstance.connection.host}`,
+      `\n MongoDB connected !! DB Host: ${connectionInstance.connection.host}`
     );
     console.log(connectionInstance);
   } catch (error) {
