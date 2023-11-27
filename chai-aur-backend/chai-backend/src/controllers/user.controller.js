bbbfe5371947eef8009670c7784f32211cd1cdb7
Fayaj Hossain <fayazhossain88@gmail.com>
2023-11-27 20:26:14 +0530

    Add user registration functionality

--- chai-aur-backend/chai-backend/src/controllers/user.controller.js
+++ chai-aur-backend/chai-backend/src/controllers/user.controller.js
@@ -1,9 +1,23 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "chai aur code",
-  });
+  //  get user details from frontend
+  const { fullName, email, username, password } = req.body;
+  console.log("email: ", email);
+
+  // validation - not empty
+
+  // Check if user already exists: username, email
+
+  //  check for images , check for avatar
+
+  //  upload them to cloudianry, avatar
+
+  //  create user object - create in db
+
+  //  remove password and refresh token field from response
+
+  //  return respone
 });
 
 export { registerUser };
