1f9c18276a8fda53a6bd16fe94db9ce1c8e260d6
Fayaj Hossain <fayazhossain88@gmail.com>
2023-11-28 21:00:10 +0530

    Fix logging statement and handle cover image upload

--- chai-aur-backend/chai-backend/src/controllers/user.controller.js
+++ chai-aur-backend/chai-backend/src/controllers/user.controller.js
@@ -7,7 +7,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
   //  get user details from frontend
   const { fullName, email, username, password } = req.body;
-  console.log("email: ", email);
+  //console.log("email: ", email);
 
   // validation - not empty
 
@@ -29,7 +29,17 @@ const registerUser = asyncHandler(async (req, res) => {
 
   //  check for images , check for avatar
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
   }
@@ -54,7 +64,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // check for user creation
 
   const createdUser = await User.findById(user._id).select(
-    "-password -refreshToken",
+    "-password -refreshToken"
   );
 
   if (!createdUser) {
